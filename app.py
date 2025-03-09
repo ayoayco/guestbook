586a3b38890a57cc71273f241fb5a25c8feed307
Ayo Ayco <ayo@ayco.io>
2025-03-09 12:36:40 +0100

    feat: update form

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from .guestbook import guestbook
 from .cache import cache
 
 app = Flask(__name__)
+app.config.from_file("config.json", load=json.load)
 cache.init_app(app, config={'CACHE_TYPE': 'SimpleCache'})
 app.register_blueprint(guestbook, url_prefix='/')
-app.config.from_file("config.json", load=json.load)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
